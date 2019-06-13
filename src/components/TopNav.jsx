d1a40a75a987c3e262f142415dc07c4b6382eac5
Yury Chebotaev <yury.79120345101@gmail.com>
2019-06-14 04:05:45 +0500

    Логика для страницы логина

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -9,27 +9,36 @@ import {
 } from 'reactstrap'
 import { Block } from '../commons/Block'
 import { Link } from 'react-router-dom'
+import { useNetlifyIdentity } from 'react-netlify-identity'
 
-export const TopNav = () => (
-  <Block name='TopNav'>
-    <Navbar color='light' light expand='md'>
-      <Container>
-        <NavbarBrand tag={Link} to='/'>
-          Пыщь-Пыщь
-        </NavbarBrand>
-        <Nav className='ml-auto' navbar>
-          <NavItem>
-            <NavLink tag={Link} to='/'>
-              Глагнэ
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to='/login'>
-              Вход
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Container>
-    </Navbar>
-  </Block>
-)
+export const TopNav = () => {
+  const { isLoggedIn } = useNetlifyIdentity(
+    process.env.REACT_APP_NETLIFY_IDENTITY_URL
+  )
+
+  return (
+    <Block name='TopNav'>
+      <Navbar color='light' light expand='md'>
+        <Container>
+          <NavbarBrand tag={Link} to='/'>
+            Пыщь-Пыщь
+          </NavbarBrand>
+          <Nav className='ml-auto' navbar>
+            <NavItem>
+              <NavLink tag={Link} to='/'>
+                Глагнэ
+              </NavLink>
+            </NavItem>
+            {!isLoggedIn && (
+              <NavItem>
+                <NavLink tag={Link} to='/login'>
+                  Вход
+                </NavLink>
+              </NavItem>
+            )}
+          </Nav>
+        </Container>
+      </Navbar>
+    </Block>
+  )
+}
--- src/forms/LoginForm.jsx
+++ src/forms/LoginForm.jsx
@@ -1,25 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles/LoginForm.scss'
+import { compose } from 'recompose'
 import { Block } from '../commons/Block'
 import { FormControls } from '../commons/FormControls'
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
+import { useNetlifyIdentity } from 'react-netlify-identity'
+import { withRouter } from 'react-router'
+import { ShowError } from '../commons/ShowError'
+import { Spinner } from '../commons/Spinner'
 
-export const LoginForm = () => {
-  const onEnter = e => {
+const enhance = compose(withRouter)
+
+export const LoginForm = enhance(({ history }) => {
+  const { _goTrueInstance, setUser } = useNetlifyIdentity(
+    process.env.REACT_APP_NETLIFY_IDENTITY_URL
+  )
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState()
+
+  const onChangeEmail = e => {
+    setEmail(e.target.value)
+  }
+
+  const onChangePassword = e => {
+    setPassword(e.target.value)
+  }
+
+  const loginUser = async (email, password, remember) => {
+    const user = _goTrueInstance.login(email, password, remember)
+    setUser(user)
+    return user
+  }
+
+  const onEnter = async e => {
     e.preventDefault()
+    try {
+      setError()
+      setLoading(true)
+      await loginUser(email, password, true)
+      history.push('/')
+    } catch (error) {
+      setError(error)
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
     <Block name='LoginForm'>
       <h1>Вход</h1>
+      {error && <ShowError error={error} />}
+      {loading && <Spinner />}
       <Form>
         <FormGroup>
           <Label for='email'>Емейл</Label>
-          <Input type='email' name='email' placeholder='vasya@pupkin.ru' />
+          <Input
+            type='email'
+            name='email'
+            placeholder='vasya@pupkin.ru'
+            value={email}
+            onChange={onChangeEmail}
+          />
         </FormGroup>
         <FormGroup>
           <Label for='password'>Пароль</Label>
-          <Input type='password' name='password' />
+          <Input
+            type='password'
+            name='password'
+            value={password}
+            onChange={onChangePassword}
+          />
         </FormGroup>
         <FormControls>
           <Button color='primary' onClick={onEnter}>
@@ -29,4 +81,4 @@ export const LoginForm = () => {
       </Form>
     </Block>
   )
-}
+})
